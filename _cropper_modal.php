95ae02b915d7f0945430fdb219776aef07831ed9
novakvova <novakvova@gmail.com>
2021-06-03 19:14:56 +0300

    Is good cropper

--- _cropper_modal.php
+++ _cropper_modal.php
@@ -1,5 +1,5 @@
 <!-- Modal -->
-<div class="modal" id="cropperModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal" data-backdrop="static" id="cropperModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
